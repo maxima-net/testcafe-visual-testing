0f8426ebe1cd08effa52f5f533c9525b149e689f
kucherov.maksim <kucherov.maksim@devexpress.com>
2017-06-01 16:09:56 +0300

    added date to screen shots path

--- ts/utils.ts
+++ ts/utils.ts
@@ -1,9 +1,17 @@
 import * as fs from "fs";
 import * as tc from "testcafe";
+import * as datefromat from "dateformat";
 import { looksSameAsync } from "../looks-same-async";
 
 
 export class ScreenComparer {
+    private static initDate: Date;
+    static get InitDate(): Date {
+        if (this.initDate === undefined) 
+            this.initDate = new Date();
+        return this.initDate;
+    }
+
     static async Compare(testController : TestController, stateName: string) {
         let ethalonPath = this.GetEthalonScreensPath(testController, stateName);
         if (!fs.existsSync(ethalonPath)){
@@ -17,7 +25,7 @@ export class ScreenComparer {
     static async CompareCore(testController : TestController, stateName: string) {
         await testController.takeScreenshot(this.GetScreensPath(testController, stateName, true, false));
         let res = await looksSameAsync(this.GetEthalonScreensPath(testController, stateName), this.GetScreenShootsPath(testController, stateName));
-        await testController.expect(!!res).eql(true, 'images is not equals');
+        await testController.expect(!!res).eql(true, `images is not equals. State: ${stateName}`);
     } 
     static async CreateEthalon(testController : TestController, stateName: string) {
         await testController.takeScreenshot(this.GetScreensPath(testController, stateName, true, true));
@@ -34,8 +42,13 @@ export class ScreenComparer {
         let screenShotPath = !ignoreRoot ? `${tc.testRun.opts.screenshotPath}/` : "";
         let fixtureName = tc.testRun.test.fixture.name;
         let testName = tc.testRun.test.name;
-        let screenFolderName = isEthalon ? "ethalon" : "current";        
+        let date = this.InitDate;
+
+        let currentDateTime = !isEthalon 
+            ? `${datefromat(date, "yyyy-dd-mm HH-MM-ss")}/` 
+            : "";
+        let screenFolderName = isEthalon ? "ethalon" : "current";
         let browserName = tc.testRun.browserConnection.browserInfo.browserName;
-        return `${screenShotPath}${fixtureName}/${testName}/${screenFolderName}/${stateName}/${browserName}.png`;
+        return `${screenShotPath}${fixtureName}/${testName}/${screenFolderName}/${currentDateTime}${stateName}/${browserName}.png`;
     }
 }
