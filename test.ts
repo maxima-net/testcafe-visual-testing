6fdb4c38d92653044d332e79a2d151ee1c4dff42
kucherov.maksim <kucherov.maksim@devexpress.com>
2017-06-01 12:36:19 +0300

    ScreenComparer creeate ethalon on demand

--- test.ts
+++ test.ts
@@ -3,16 +3,9 @@ import * as tc from "testcafe";
 import { looksSameAsync } from "./looks-same-async";
 import { ScreenComparer } from './ts/utils';
 
-const ethalonPath = 'ethalon/my-first-test.png';
-const currentPath = 'current/my-first-test.png';
-
 fixture(`Getting Started`)
     .page(`./index.html`)
-    .beforeEach(async t  => {
-        await t.resizeWindow(800, 600);
-        if (!fs.existsSync('img/' + ethalonPath))
-            await t.takeScreenshot(ethalonPath);
-    });
+    .beforeEach(async t  => await t.resizeWindow(800, 600));
 
 test('My first test', async t => {
     await ScreenComparer.Compare(t, 'stait0');
--- ts/utils.ts
+++ ts/utils.ts
@@ -4,10 +4,23 @@ import { looksSameAsync } from "../looks-same-async";
 
 
 export class ScreenComparer {
-    static async Compare(testController : TestController, stateName: string) : Promise<void> {
+    static async Compare(testController : TestController, stateName: string) {
+        let ethalonPath = this.GetEthalonScreensPath(testController, stateName);
+        if (!fs.existsSync(ethalonPath)){
+            await this.CreateEthalon(testController, stateName);
+            await testController.expect(fs.existsSync(ethalonPath)).eql(true, "can't create ethalon");
+        }
+        else
+            await this.CompareCore(testController, stateName);
+    }
+
+    static async CompareCore(testController : TestController, stateName: string) {
         await testController.takeScreenshot(this.GetScreensPath(testController, stateName, true, false));
         let res = await looksSameAsync(this.GetEthalonScreensPath(testController, stateName), this.GetScreenShootsPath(testController, stateName));
         await testController.expect(!!res).eql(true, 'images is not equals');
+    } 
+    static async CreateEthalon(testController : TestController, stateName: string) {
+        await testController.takeScreenshot(this.GetScreensPath(testController, stateName, true, true));
     }
 
     static GetEthalonScreensPath(testController : TestController, stateName: string) : string {
@@ -18,11 +31,11 @@ export class ScreenComparer {
     }
     static GetScreensPath(testController : TestController, stateName: string, ignoreRoot: boolean, isEthalon: boolean) : string {
         let tc = testController as any;
-        let screenShootPath = !ignoreRoot ? `${tc.ctx.opts.screenShootPath}/` : "";
+        let screenShotPath = !ignoreRoot ? `${tc.testRun.opts.screenshotPath}/` : "";
         let fixtureName = tc.testRun.test.fixture.name;
         let testName = tc.testRun.test.name;
+        let screenFolderName = isEthalon ? "ethalon" : "current";        
         let browserName = tc.testRun.browserConnection.browserInfo.browserName;
-        let screenFolderName = isEthalon ? "ethalon" : "current";
-        return `${screenShootPath}${fixtureName}/${testName}/${screenFolderName}/${stateName}.png`;
+        return `${screenShotPath}${fixtureName}/${testName}/${screenFolderName}/${stateName}/${browserName}.png`;
     }
 }
